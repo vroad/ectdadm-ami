395aa21886a69e0f89515bd2e1887e6f8b70635d
vroad <xvroad@gmail.com>
2019-04-14 15:51:52 +0900

    Change installer image to golang

--- tasks/install-etcdadm.sh
+++ tasks/install-etcdadm.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 ETCDADM_INSTALL_SCRIPT=`cat <<EOT
-apt-get update \
-&& apt-get install -y --no-install-recommends ca-certificates git golang-1.10 \
-&& /usr/lib/go-1.10/bin/go get -u sigs.k8s.io/etcdadm \
-&& cp /root/go/bin/etcdadm /out/
+go get -u sigs.k8s.io/etcdadm \
+&& cp /go/bin/etcdadm /out/
 EOT
 `
 
-BUILDER_IMAGE=ubuntu:18.04
+INSTALLER_IMAGE=golang:1.10
 
 docker run --rm \
     -v /opt/bin:/out \
-    $BUILDER_IMAGE /bin/bash -c "$ETCDADM_INSTALL_SCRIPT"
-docker image rm $BUILDER_IMAGE
+    $INSTALLER_IMAGE /bin/bash -c "$ETCDADM_INSTALL_SCRIPT"
+docker image rm $INSTALLER_IMAGE
